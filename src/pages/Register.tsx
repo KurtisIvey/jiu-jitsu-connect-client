1f2c2a5c4a53f9d7902167427d4e98931ff7baa1
Kitsunebackfire <kurtiveycodes@gmail.com>
2023-03-02 17:23:08 -0600

    test change

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -78,7 +78,7 @@ function Register({}: Props) {
                 className="w-full text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 
                   font-semibold rounded-lg text-sm px-5 py-2.5 text-center "
               >
-                Create Account
+                Create Accountf
               </button>
               <p className="text-sm font-light text-gray-500">
                 Already have an account?{" "}
