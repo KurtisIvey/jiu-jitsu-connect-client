084d571749830224ed368eceda03b99234e8af04
Kitsunebackfire <kurtiveycodes@gmail.com>
2023-04-22 15:42:13 -0500

    remove hoc from app.tsx due to inability to get working

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { HashRouter, Route, Routes, useParams } from "react-router-dom";
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Profile from "./pages/Profile";
@@ -8,24 +8,13 @@ import Settings from "./pages/Settings";
 import FriendRequests from "./pages/FriendRequests";
 import Friends from "./pages/Friends";
 
-// redux
-import type { RootState } from "./reduxStore/store";
-import { useSelector } from "react-redux";
-
 function App() {
-  // redux authenticated state to protect routes
-  const authenticated = useSelector(
-    (state: RootState) => state.user.authenticated
-  );
-
-  //let { id } = useParams();
   return (
     <div className="bg-[#f5f3f3] font-roboto min-h-screen ">
       <HashRouter>
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          {/* <Route path="/profile/:profileId" element={<Profile />} /> */}
           <Route path="/profile/:id" element={<Profile />} />
           <Route path="/home" element={<Home />} />
           <Route path="/settings" element={<Settings />} />
