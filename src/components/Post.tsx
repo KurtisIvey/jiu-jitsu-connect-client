ff2e1dbc9b0c07eec00fa337136cff9a64f04782
Kitsunebackfire <kurtiveycodes@gmail.com>
2023-06-27 12:14:14 -0500

    add delete post button to conditionally render if post by user

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -4,6 +4,9 @@ import Like from "./Like";
 import { convertDateTime } from "../helpers/ConvertDateTime";
 import CommentContainer from "./CommentContainer";
 import ProfileImageLink from "./ProfileImageLink";
+import { FaRegTrashAlt } from "react-icons/fa";
+import { useSelector } from "react-redux";
+import { RootState } from "../reduxStore/store";
 
 type Props = {
   id: string;
@@ -14,11 +17,22 @@ type Props = {
 };
 
 function Post(props: Props) {
+  const loggedInUserId = useSelector((state: RootState) => state.user.id);
+
   return (
-    <section className="container  p-3 lg:p-0 lg:mx-0 flex flex-col">
+    <section className="container  p-3 lg:p-0 lg:mx-0 flex flex-col ">
       <div className="mx-2 lg:mx-0 shadow-lg p-2 rounded-lg bg-white space-y-2">
         {/*top row of post, image name, date*/}
-        <div className="flex flex-row space-x-2">
+        <div className="flex flex-row  relative ">
+          {/* delete btn */}
+          {loggedInUserId === props.author._id && (
+            <button
+              className="absolute right-2 top-1/2 transform -translate-y-1/2 text-red-600 hover:text-red-700"
+              aria-label="delete post"
+            >
+              <FaRegTrashAlt />
+            </button>
+          )}
           <ProfileImageLink
             profilePicUrl={props.author.profilePicUrl}
             profileId={props.author._id}
